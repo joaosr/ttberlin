e259cc89c595be1c5fa5a900feb6e469180ad402
Joao Soares <joaosr@Joaos-MacBook-Pro.local>
2018-03-13 11:33:45 +0100

    Fixed base layer in the map

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 var map = L.map('map').setView([52.529, 13.341], 14);
 var tischtennisPoints = L.featureGroup();
-L.tileLayer('https://api.mapbox.com/styles/v1/joaosr/cj92tr4lc42p52spv77xobif7/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9hb3NyIiwiYSI6ImNqOHZuMmg1ejEzbTAyd3JwdmJmNHFhdXEifQ.ZPTO-d3wGLEqu3VfOSzE6Q', {
+L.tileLayer('https://api.mapbox.com/styles/v1/joaosr/cj92tr4lc42p52spv77xobif7/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9hb3NyIiwiYSI6ImNqZThwanBqMjAxaTAyeHM2N2tiZ2xyOHgifQ.UUvBcUdzfX9U3D7OvKmAlQ', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   maxZoom: 18
 }).addTo(map);
